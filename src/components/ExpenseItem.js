e239431c740d08252a705a15c7b5c8cd7ff62dc5
Peter A. Cabel <PCabel@TireBuyer.com>
2023-08-02 10:39:12 -0400

    Added static variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 0, 3);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 123.45;
+
   return (
-    <div>
-      <div>06/02/2023</div>
-      <div>
-        <div>Car Insurance</div>
-        <div>$123.34</div>
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
