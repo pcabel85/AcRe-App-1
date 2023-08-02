7f015ab11e4c26498d7d5ea55f59711f85793c29
Peter A. Cabel <PCabel@TireBuyer.com>
2023-08-02 10:05:40 -0400

    First commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>06/02/2023</div>
+      <div>
+        <div>Car Insurance</div>
+        <div>$123.34</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
