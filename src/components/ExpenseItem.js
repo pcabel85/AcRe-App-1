7ef2b026c173a5af3ac7edab6ef61b43086d5064
Peter A. Cabel <PCabel@TireBuyer.com>
2023-08-02 15:59:04 -0400

    Props and destructuring

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,19 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 0, 3);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 123.45;
-
+function ExpenseItem({date, title, amount}) { //Props being descructured into local variables (date, time, amount)
+    const month = date.toLocaleString('en-US', {month: 'long'});
+    const day = date.toLocaleString('en-US', {day: '2-digit'});
+    const year = date.getFullYear();
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${amount}</div>
       </div>
     </div>
   );
